811371520c5abc621742140605bc811c89734df5
Julia Zyabrveva <juliazyabreva@gmail.com>
2016-03-14 23:24:07 +0200

    js-2commit

--- javascript1-2/js2/js/script.js
+++ javascript1-2/js2/js/script.js
@@ -1,20 +1,26 @@
+var names = new Array(5);
+var userName;
+var flag;
 
-var arr = ["Юлия", "Максим", "Дмитрий", "Виталий", "Олег"];
-
-
-var userName = prompt('Введите своё имя^');
-var flag = false;
-
-
-
-for (var i = 0; i < arr.length; i++) {
-  if (arr[i] === userName){
-    flag = true;
-  }
+for (var i = 0; i < names.length; i++) {
+	if (i == 0) {
+		names[i] = prompt('Введите имя:');		
+	} else {
+		names[i] = prompt('Введите следующее имя:');
+	}
 }
 
-if (flag){
-  alert(userName + ', вы успешно вошли.');
-} else {
-  alert('Такого пользователя не существует.')
+userName = prompt('Введите имя пользователя:');
+
+for (var i = 0; i < names.length; i++) {
+	if (names[i] == userName) {
+		flag = true;
+	}
 }
+
+if (flag) {
+		alert(userName + ', вы успешно вошли!');
+} else{
+		alert('Ошибка! Неверное имя пользователя.');
+}
+
