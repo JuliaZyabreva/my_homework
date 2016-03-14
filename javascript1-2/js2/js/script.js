c922518d92ef57df91f1ee71868ef49ede359778
Julia Zyabrveva <juliazyabreva@gmail.com>
2016-03-14 20:17:43 +0200

    js-1commit

---
+++ javascript1-2/js2/js/script.js
@@ -0,0 +1,20 @@
+
+var arr = ["Юлия", "Максим", "Дмитрий", "Виталий", "Олег"];
+
+
+var userName = prompt('Введите своё имя^');
+var flag = false;
+
+
+
+for (var i = 0; i < arr.length; i++) {
+  if (arr[i] === userName){
+    flag = true;
+  }
+}
+
+if (flag){
+  alert(userName + ', вы успешно вошли.');
+} else {
+  alert('Такого пользователя не существует.')
+}
